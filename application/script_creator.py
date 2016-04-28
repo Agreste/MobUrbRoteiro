7c1d28d91040b6bb2667ff456231516eec2162bb
Edgar Zanella Alvarenga <e@vaz.io>
2016-04-28 14:48:25 +0200

    Adding slug video creator

--- application/script_creator.py
+++ application/script_creator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import string
 import pickle
 
 videos_file = open('application/videos.dict')
@@ -13,11 +14,47 @@ videos_urls = pickle.load(videos_urls_file)
 borba_urls_file = open('application/borba_urls.dict')
 borba_urls = pickle.load(borba_urls_file)
 
+borba_durations_file = open('application/borba_durations.dict')
+borba_durations = pickle.load(borba_durations_file)
+
+borba_ids_file = open('application/borba_ids.dict')
+borba_ids = pickle.load(borba_ids_file)
+
+video_durations_file = open('application/video_durations.dict')
+video_durations = pickle.load(video_durations_file)
+
+video_ids_file = open('application/video_ids.dict')
+video_ids = pickle.load(video_ids_file)
+
 videos_weights_file = open('application/videos_weights.dict')
 videos_weights = pickle.load(videos_weights_file)
 
 videos_subs_file = open('application/videos_subs.dict')
 videos_subs = pickle.load(videos_subs_file)
+
+# Symbols used for URL slug reation
+sym = string.ascii_letters + string.digits[1:]
+
+def create_slug(lista):
+    slug = ''
+    for num in  lista:
+        if num < 61:
+            slug += sym[num]
+        else:
+            slug += '0' + sym[num - 61]
+    return slug
+
+def decode_slug(slug):
+    lista = []
+    a = 0
+    for c in slug:
+        if c == '0':
+           a = 61
+           continue
+        else:
+           lista += [a + sym.index(c)]
+           a = 0
+    return lista
 
 def seleciona_fechamento(abertura):
     abertura_nome = abertura.split(' ')[0]
@@ -30,6 +67,8 @@ def seleciona_proximo(video_atual):
     return proximo_video
 
 def cria_roteiro(numero_maximo = 7, resolution="1280x720"):
+    duracao_total = 0
+
     inicio = {"1280x720": "https://player.vimeo.com/external/159699169.hd.mp4?s=ae62327392ef36f532a69ace0304646099802272&profile_id=113",
                "960x540": "https://player.vimeo.com/external/159699169.sd.mp4?s=002561d1556097cc3c385431c8697d3d82f8b044&profile_id=165",
                "640x360": "https://player.vimeo.com/external/159699169.sd.mp4?s=002561d1556097cc3c385431c8697d3d82f8b044&profile_id=164"}
@@ -40,15 +79,24 @@ def cria_roteiro(numero_maximo = 7, resolution="1280x720"):
 
     roteiro = [(inicio[resolution], 'NOSUB')]
     abertura = np.random.choice(tags['Abertura'])
+
+    duracao_total += float(video_durations[video_ids[abertura]])
+
     roteiro.append((videos_urls[resolution][abertura], videos_subs.get(abertura, 'NOSUB')))
 
     atual = abertura
     for i in range(numero_maximo):
         proximo = seleciona_proximo(atual)
+
+        duracao_total += float(video_durations[video_ids[proximo]])
+
         roteiro.append((videos_urls[resolution][proximo], videos_subs.get(proximo, 'NOSUB')))
         atual = proximo
 
     fechamento = np.random.choice(seleciona_fechamento(abertura))
+
+    duracao_total += float(video_durations[video_ids[fechamento]])
+
     roteiro.append((videos_urls[resolution][fechamento], videos_subs.get(fechamento, 'NOSUB')))
     roteiro.append((finalizacao[resolution], 'NOSUB'))
 
@@ -62,5 +110,15 @@ def cria_roteiro(numero_maximo = 7, resolution="1280x720"):
 
     for i, indice in enumerate(indices_borba):
         roteiro.insert(indice + i, (borba_urls[resolution][trechos_borba[i]], 'NOSUB'))
+
+        duracao_total += float(borba_durations[borba_ids[trechos_borba[i]]])
+
+    print duracao_total
+    minutos = int(duracao_total)/60
+    segundos = int(duracao_total)%60
+    if segundos != 0:
+        print '{}m{}s'.format(minutos, segundos)
+    else:
+        print '{}m'.format(minutos)
 
     return roteiro

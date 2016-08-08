8229ab79f6a4690b55e1aab511e681400a7607f6
Edgar Zanella Alvarenga <e@vaz.io>
2016-08-08 14:34:00 +0200

    Code for subtitles based on domain used

--- application/static/javascripts/video_player.js
+++ application/static/javascripts/video_player.js
@@ -5,6 +5,22 @@ var videoSource;
 var playlist;
 
 function changeVideo(event, playing, video_num) {
+    validLanguages = ['en', 'fr', 'de', 'es'];
+    lang = '';
+
+    langIdx = document.location.hostname.indexOf('.')
+
+    if (langIdx == 2) {
+        langUrl = document.location.hostname.substring(0, langIdx)
+        console.log(langUrl);
+        if (validLanguages.indexOf(langUrl) != -1) {
+            lang = langUrl;
+            console.log(lang);
+        }
+    }
+
+    console.log(lang);
+
     if (typeof(video_num) == "number") {
         video_idx = video_num;
     }
@@ -12,8 +28,20 @@ function changeVideo(event, playing, video_num) {
     if (video_idx < playlist['sequence'].length && video_idx > 0) {
         window.history.replaceState({}, '', playlist['slug'] + '#' + video_idx.toString());
     } else {
-        window.history.replaceState({}, '', playlist['slug']);
-        video_idx = 0;
+        if (video_idx == playlist['sequence'].length) {
+            var options = $('<div><img src="http://localhost:5000/static/img/shuffle.png"></div>');
+            var video = $('video')[0];
+            options.css('width', video.clientWidth);
+            options.css('height', video.clientHeight);
+            options.addClass('options');
+            $('video').replaceWith(options);
+            return;
+        } else {
+            window.history.replaceState({}, '', playlist['slug']);
+            video_idx = 0;
+        }
+
+
     }
 
     videoSource.setAttribute('src', playlist['sequence'][video_idx]['url']);
@@ -27,7 +55,11 @@ function changeVideo(event, playing, video_num) {
             track.kind = "captions";
             track.label = "Português";
             track.srclang = "pt";
-            track.src = "/static/subtitles/" + playlist['sequence'][video_idx-1]['sub'];
+            if (lang == '') {
+                track.src = "/static/subtitles/" + playlist['sequence'][video_idx-1]['sub'];
+            } else {
+                track.src = "/static/subtitles/" + lang + '/' + playlist['sequence'][video_idx-1]['sub'];
+            }
             track.addEventListener("load", function() {
                 this.mode = "showing";
                 videoPlaying.textTracks[0].mode = "showing"; // thanks Firefox
@@ -54,8 +86,10 @@ function changeVideo(event, playing, video_num) {
 
 startVideos = function(videoId, pl) {
     playlist = pl;
+    lang = '';
     locationHash = window.location.hash.substring(1);
-    video_idx = 0;
+
+        video_idx = 0;
 
     if (locationHash != "") {
         video_idx = Number(locationHash);
@@ -72,8 +106,6 @@ startVideos = function(videoId, pl) {
     videos_menu = $('#videos-menu > a');
 
     videos_menu.attr('href', videos_menu.attr('href') + `/${playlist['slug']}`);
-
-
 
     videoPlaying.addEventListener('ended', changeVideoEv, false);
 
